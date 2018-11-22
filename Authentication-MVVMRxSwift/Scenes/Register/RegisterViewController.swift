2fa3ca0945e602880b3236919a2dce41665abd9a
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2018-11-22 16:08:45 +0200

    add register screen ui and outlets

---
+++ Authentication-MVVMRxSwift/Scenes/Register/RegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterViewController.swift
+//  Authentication-MVVMRxSwift
+//
+//  Created by Karim Ebrahem on 11/22/18.
+//  Copyright © 2018 KarimEbrahem. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
