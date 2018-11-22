5921f690e95022e78c0f89964573b6e3ff4c264b
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2018-11-22 16:19:25 +0200

    add forget password screen ui and outlets

---
+++ Authentication-MVVMRxSwift/Scenes/Forget Password/ForgetPasswordViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ForgetPasswordViewController.swift
+//  Authentication-MVVMRxSwift
+//
+//  Created by Karim Ebrahem on 11/22/18.
+//  Copyright © 2018 KarimEbrahem. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
